c7223ed4b92f47d7d95c1f8e5e1abbc1def33bd1
Korin <kav02@outlook.com>
2024-02-20 15:23:00 -0600

    Agregar estadistica

--- src/index.js
+++ src/index.js
@@ -15,9 +15,9 @@ import { Groupal } from "./../views/groupal.js";
 
 ///1.- Definir rutas en router.
 const routes = {
-  "/": Home,
-  "/error": Error,
   "/welcome": Welcome,
+  "/error": Error,
+  "/home": Home,
   "/card": Card,
   "/individual": Individual,
   "/groupal": Groupal,
@@ -34,17 +34,17 @@ document.addEventListener("DOMContentLoaded", (event) => {
   onURLChange(event.target.location.pathname);
 });
 
-// //Carga inicial
-// window.addEventListener("DOMContentLoaded", () => {
-//   // Establecer el elemento root
-//   const root = document.getElementById("root");
-//   setRootElement(root);
-//   //Accesar la url inicial
-//   const pathname = window.location.pathname;
-//   //Navegacion inicial
-//   navigateTo(pathname); // Use your router's navigation function
-//   // invocar onURLChange
-//   window.addEventListener("popstate", (event) => {
-//     onURLChange(event.location);
-//   });
-// });
+//Carga inicial
+window.addEventListener("DOMContentLoaded", () => {
+  // Establecer el elemento root
+  const root = document.getElementById("root");
+  setRootElement(root);
+  //Accesar la url inicial
+  const pathname = window.location.pathname;
+  //Navegacion inicial
+  navigateTo(pathname); // Use your router's navigation function
+  // invocar onURLChange
+  window.addEventListener("popstate", (event) => {
+    onURLChange(event.location);
+  });
+});
